c7a1e25776296f731225483cb857c30a7765272e
Märt Konsap <mrt.konsap@gmail.com>
2017-10-12 13:05:59 +0300

    Implemented very basic token querying

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -4,7 +4,7 @@
 from configparser import ConfigParser
 from telegram.ext import Updater, MessageHandler, Filters
 import __init__ as init
-from common.request import BackendConnection
+from common.request import BackendConnection, InvalidTokenException
 
 
 log = init.getLogger(__name__)
@@ -12,31 +12,52 @@ log = init.getLogger(__name__)
 log.info("Read configuration")
 config = ConfigParser()
 config.read('../configuration.ini')
-token = config['telegram']['token']
+telegram_token = config['telegram']['token']
 url   = config['backend']['url'] + ':' + config['backend']['port']
 
 conn = BackendConnection(url)
 
+tokens = {}
+
 
 def query(bot, update):
+    user_id = update.effective_user.id
+    token = None if user_id not in tokens else tokens.get(user_id)
+
+    log.debug("user_id: "  + str(user_id) + " token: " + str(token))
     log.debug("IN: " + update.message.text)
     if update.message.text[:1] == '!':
         message = update.message.text[1:]
         log.info("IN:  " + message)
-        response = conn.query(message)
-        log.info("OUT: " + response['message'])
-        update.message.reply_text(response['message'])
+        try:
+            response = conn.query(
+                q=message,
+                token=token
+            )
+            response = response['message']
+        except InvalidTokenException:
+            log.info("Needed token to query Waldur, asking user for token.")
+            response = "Needed token to query Waldur API. " \
+                       "Token was either invalid or missing. " \
+                       "Please send token like this '?<TOKEN>'"
+
+        update.message.reply_text(response)
+        log.info("OUT: " + response)
+    elif update.message.text[:1] == '?':
+        log.info("Received token from user " + str(user_id) + " with a length of " + str(len(update.message.text[1:])))
+        tokens[user_id] = update.message.text[1:]
+        update.message.reply_text("Thanks")
 
 
 def main():
     log.info("Initializing bot")
-    updater = Updater(token)
+    updater = Updater(telegram_token)
 
     dp = updater.dispatcher
     log.info("Adding handlers")
     # responds to any message that starts with '!'
     dp.add_handler(MessageHandler(Filters.text, query))
-
+    # todo add error handler
     log.info("Starting polling")
     updater.start_polling()
 
