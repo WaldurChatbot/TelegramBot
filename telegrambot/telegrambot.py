b9914c1a304373ece17b02af10b312e90907d1f1
Märt Konsap <mrt.konsap@gmail.com>
2017-10-24 14:05:12 +0300

    Added error handler, very basic, needs work

--- telegrambot/telegrambot.py
+++ telegrambot/telegrambot.py
@@ -5,6 +5,7 @@ from configparser import ConfigParser
 from telegram.ext import Updater, MessageHandler, Filters
 import __init__ as init
 from common.request import BackendConnection
+import traceback
 
 
 log = init.getLogger(__name__)
@@ -28,11 +29,19 @@ def query(bot, update):
         update.message.reply_text(response)
 
 
+def error_callback(bot, update, error):
+    try:
+        raise error
+    except:
+        for line in traceback.format_exc().split("\n"): log.error(line)
+
+
 def main():
     log.info("Initializing bot")
     updater = Updater(telegram_token)
 
     dp = updater.dispatcher
+    dp.add_error_handler(error_callback)
     log.info("Adding handlers")
     # responds to any message that starts with '!'
     dp.add_handler(MessageHandler(Filters.text, query))
